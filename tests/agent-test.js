70ec97569061f97686e0ba2c47747c4031388783
arq-gabo <jgabrielguerral@gmail.com>
2021-01-04 19:12:05 -0400

    [FEATURE]Implementación de findbyId y pruebas en el servicio Agent

--- tests/agent-test.js
+++ tests/agent-test.js
@@ -22,9 +22,15 @@ let sandbox = null;
 
 test.beforeEach(async () => {
     sandbox = sinon.createSandbox();
+
     AgentStub = {
         hasMany: sandbox.spy()
     }
+
+    // Model findById Stub
+    AgentStub.findById = sandbox.stub();
+    AgentStub.findById.withArgs(id)
+        .returns(Promise.resolve(agentFixture.byId(id)));
 
     const setupDatabase = proxyquire('../', {
         './models/agent': () => AgentStub,
@@ -50,6 +56,10 @@ test.serial('Setup', t => {
 
 test.serial('AgentfindById', async t => {
     let agent = await db.Agent.findById(id);
+
+    t.true(AgentStub.findById.called, 'findById should be called on model')
+    t.true(AgentStub.findById.calledOnce, 'findById should be called once')
+    t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
 
     t.deepEqual(agent, agentFixture.byId(id), 'Should be the same')
 })
