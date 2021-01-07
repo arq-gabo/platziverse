728ae3adf33696106a7830cf04c919eca4dda857
arq-gabo <jgabrielguerral@gmail.com>
2021-01-07 17:20:53 -0400

    [FEATURE]Como recibir mensajes

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const debug = require('debug')
+const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
@@ -12,12 +12,36 @@ const backend = {
 }
 
 const settings = {
-    port: 6379,
+    port: 1883,
     backend
 }
 
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', client => {
+    debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+    debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+    debug(`Received: ${packet.topic}`)
+    debug(`Payload: ${packet.payload}`)
+})
+
 server.on('ready', () => {
     console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
+
+server.on('error', handleFatalError)
+
+function handleFatalError(err){
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
