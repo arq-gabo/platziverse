cb2e0248ef6d3fe4fe06429a042c44610bf28e78
arq-gabo <jgabrielguerral@gmail.com>
2021-01-11 15:05:25 -0500

    [FEATURE]Implementación de manejo de errores con express

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -3,6 +3,7 @@
 const http = require('http')
 const chalk = require('chalk')
 const express = require('express')
+const debug = require('debug')
 
 const api = require('./api')
 
@@ -11,6 +12,27 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/api', api)
+
+//Express error Handler
+
+app.use((err, req, res, next) => {
+    debug(`Error: ${err.message}`)
+
+    if(err.message.match(/not found/)){
+        return res.status(404).send({ error: err.message })
+    }
+
+    res.status(500).send({ error: err.message })
+})
+
+function handleFatalError(err){
+    console.error(`${chalk.red('[Fatal Error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
     console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
