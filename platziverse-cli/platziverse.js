c12ac6b04fdc08624a26df003cef177abbaad055
arq-gabo <jgabrielguerral@gmail.com>
2021-01-16 18:39:41 -0500

    [FEATURE]Implementación del layout básico con Blessed Contrib

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -2,17 +2,32 @@
 
 'use strict'
 
-// const minimist = require('minimist')
-// console.log('Hello Platziverse!!!')
-// const argv = minimist(process.argv)
-// console.log(argv.host)
-// console.log(argv.name)
+/* eslint new-cap: "off" */
 
-const args = require('args')
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
 
-args
-  .option('port', 'The port on which the app will be running', 3000)
-  .option('reload', 'Enable/disable livereloading')
-  .command('serve', 'Serve your static site', ['s'])
- 
-const flags = args.parse(process.argv)
+const screen = blessed.screen()
+
+const grid = new contrib.grid({
+    rows: 1,
+    cols: 4,
+    screen
+})
+
+const tree = grid.set(0, 0, 1, 1, contrib.tree, {
+    label: 'Connected Agents'
+})
+
+const line = grid.set(0, 1, 1, 3, contrib.line, {
+    label: 'Metric',
+    showLegent: true,
+    minY: 0,
+    xPadding: 5
+})
+
+screen.key(['scape', 'q', 'C-c'], (ch, key) => {
+    process.exit(0)
+})
+
+screen.render()
