b0f1eba66a84080d36a559e4821a7c9a22cfdda4
arq-gabo <jgabrielguerral@gmail.com>
2021-01-17 12:17:25 -0500

    [FEATURE]Integración con el agente de monitoreo 2

--- platziverse-cli/platziverse.js
+++ platziverse-cli/platziverse.js
@@ -7,6 +7,7 @@
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
 const PlatziverseAgent = require('platziverse-agent')
+const moment = require('moment')
 
 const agent = new PlatziverseAgent()
 const screen = blessed.screen()
@@ -41,6 +42,48 @@ agent.on('agent/connected', payload => {
     renderData()
 })
 
+agent.on('agent/disconnected', payload => {
+    const { uuid } = payload.agent
+
+    if(agents.has(uuid)){
+        agents.delete(uuid)
+        agentMetrics.delete(uuid)
+    }
+
+    renderData()
+})
+
+agent.on('agent/message', payload => {
+    const { uuid } = payload.agent
+    const { timestamp } = payload
+
+    if(!agents.has(uuid)){
+        agents.set(uuid, payload.agent)
+        agentMetrics.set(uuid, {})
+    }
+
+    const metrics = agentMetrics.get(uuid)
+
+    payload.metrics.forEach(m => {
+        const { type, value } = m
+
+        if(!Array.isArray(metrics[type])){
+            metrics[type] = []
+        }
+
+        const length = metrics[type].length
+        if(length >= 20){
+            metrics[type].shift()
+        }
+
+        metrics[type].push({
+            value,
+            timestamp: moment(timestamp).format('HH:mm:ss')
+        })
+    })
+    renderData()
+})
+
 function renderData(){
     const treeData = {}
 
@@ -51,6 +94,19 @@ function renderData(){
             agent: true,
             children: {}
         }
+
+        const metrics = agentMetrics.get(uuid)
+        
+        Object.keys(metrics).forEach(type => {
+            const metric = {
+                uuid,
+                type,
+                metric: true
+            }
+
+            const metricName = ` ${type} `
+            treeData[title].children[metricName]  = metric
+        })
     }
     tree.setData({
         extended: true,
@@ -64,4 +120,5 @@ screen.key(['scape', 'q', 'C-c'], (ch, key) => {
 })
 
 agent.connect()
+tree.focus()
 screen.render()
